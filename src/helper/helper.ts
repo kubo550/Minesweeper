78b5186cc5256f356a4162e7a8762104808d072b
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-28 00:37:16 +0100

    update imports

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -1,69 +1,14 @@
-// ^ Types
-export interface BoardType {
+import { countFlagged } from "./countFlagged"
+import { createGameBoard } from "./createBoard"
+import { reveal } from "./reveal"
+
+export interface Board {
+    y: number;
+    x: number;
+    active: boolean;
     clicked: boolean;
     isBomb: boolean;
     flagged: boolean;
-    x: number;
-    y: number;
     neighbors: number;
 }
-
-
-
-// - Functions
-const countNeighbors = (b: BoardType[][], j: number, i: number) => {
-    if (b[j][i].isBomb) return -1;
-    let neighborCount = 0;
-    for (let y = i - 1; y <= i + 1; y++) {
-        for (let x = j - 1; x <= j + 1; x++) {
-            if (x >= 0 && x <= 9 && y >= 0 && y <= 9) {
-                const neighbor = b[x][y];
-                if (neighbor.isBomb) neighborCount++;
-            }
-        }
-    }
-    return neighborCount;
-};
-
-export const newBoard = (width: number, height: number): BoardType[][] => {
-    const board = Array(height)
-        .fill(null)
-        .map((_, y) =>
-            new Array(width).fill(null).map((_, x) => ({
-                clicked: false,
-                flagged: false,
-                isBomb: Math.random() > 0.84,
-                x,
-                y,
-                neighbors: 0,
-            }))
-        );
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            board[j][i].neighbors = countNeighbors(board, j, i);
-        }
-    }
-    return board;
-};
-
-
-export const reveal = (board: BoardType[][], y: number, x: number) => {
-    const currentBox = board[y][x];
-    currentBox.clicked = true;
-    if (currentBox.neighbors === 0) {
-        floodFilled(board, y, x);
-    }
-};
-
-const floodFilled = (board: BoardType[][], y: number, x: number) => {
-    for (let i = y - 1; i <= y + 1; i++) {
-        for (let j = x - 1; j <= x + 1; j++) {
-            if (i >= 0 && i <= 9 && j >= 0 && j <= 9) {
-                const neighbor = board[i][j];
-                if (!neighbor.isBomb && !neighbor.clicked) {
-                    reveal(board, i, j);
-                }
-            }
-        }
-    }
-};
+export { createGameBoard, countFlagged, reveal }
