88248c1225d4a292e914baa8856924337e396cba
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-28 00:35:23 +0100

    Add Stats

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Box, { EventType } from "./components/Box/Box";
-import { newBoard, reveal } from "./helper/helper";
+import PopupContainer from "./components/PopupContainer/PopupContainer";
+import { createGameBoard, countFlagged, reveal } from "./helper/helper";
 
 const Saper = () => {
-  const [board, setBoard] = useState(newBoard(10, 10));
+  const [maxBombs] = useState(5);
+  const [board, setBoard] = useState(createGameBoard(15, 15, maxBombs));
   const [playable, setPlayable] = useState(true);
 
   const handleClick = (x: number, y: number, event: EventType) => {
@@ -23,23 +25,44 @@ const Saper = () => {
     }
     setBoard(newBoard);
   };
+
+  const [flaggedCount, correctFlagged] = countFlagged(board);
+
   const gameOver = () => {
     const newBoard = [...board];
     newBoard.map(row =>
+      // ^ Set Wrong Flagged
       row.map(col => (col.isBomb ? (col.flagged ? null : (col.clicked = true)) : null))
     );
     setBoard(newBoard);
     setPlayable(false);
   };
+
+  if (playable && correctFlagged === maxBombs) {
+    gameOver();
+  }
+
+  const newGame = () => {
+    setBoard(createGameBoard(15, 15, maxBombs));
+    setPlayable(true);
+  };
+
   return (
-    <div className='board-container'>
-      {board.map((row, y) =>
-        row.map((cell, x) => (
-          <Box key={x + 1 * y + 1} x={x} y={y} cell={cell} handleClick={handleClick} />
-        ))
-      )}
-      {/* {!playable && <PopupContainer newGame={newGame} />} */}
-    </div>
+    <>
+      <div className='head'>
+        <h3>00:01 ⏲ </h3>
+        <h2>Mine Swipper</h2>
+        <h3> {maxBombs - flaggedCount} 💣 </h3>
+      </div>
+      <div className='board-container'>
+        {board.map((row, y) =>
+          row.map((cell, x) => (
+            <Box key={x + 1 * y + 1} x={x} y={y} cell={cell} handleClick={handleClick} />
+          ))
+        )}
+        {!playable && <PopupContainer newGame={newGame} />}
+      </div>
+    </>
   );
 };
 
